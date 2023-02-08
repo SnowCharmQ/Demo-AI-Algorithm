5d99d354e751070eed0f4fad4a8a2a521e4d6e28
SnowCharm <619022098@qq.com>
2023-02-08 11:59:41 +0800

    add GAN model

---
+++ GAN/model.py
@@ -0,0 +1,121 @@
+# coding:utf8
+import numpy as np
+from torch import nn
+
+nz = 100  # 噪声维度
+ngf = 64  # 生成器feature map数
+ndf = 64  # 判别器feature map数
+
+
+class Generator(nn.Module):
+    """
+    生成器定义
+    """
+
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.main = nn.Sequential(
+            # 输入是一个nz维度的噪声，我们可以认为它是一个1*1*nz的feature map
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # 上一步的输出形状：(ngf*8) x 4 x 4
+
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # 上一步的输出形状： (ngf*4) x 8 x 8
+
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # 上一步的输出形状： (ngf*2) x 16 x 16
+
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # 上一步的输出形状：(ngf) x 32 x 32
+
+            nn.ConvTranspose2d(ngf, 3, 5, 3, 1, bias=False),
+            nn.Tanh()  # 输出范围 -1~1 故而采用Tanh
+            # 输出形状：3 x 96 x 96
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class Discriminator(nn.Module):
+    """
+    判别器定义
+    """
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            # 输入 3 x 96 x 96
+            nn.Conv2d(3, ndf, 5, 3, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 输出 (ndf) x 32 x 32
+
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 输出 (ndf*2) x 16 x 16
+
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 输出 (ndf*4) x 8 x 8
+
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 输出 (ndf*8) x 4 x 4
+
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()  # 输出一个数(概率)
+        )
+
+    def forward(self, input):
+        return self.main(input).view(-1)
+
+
+class AverageValueMeter:
+    def __init__(self):
+        super(AverageValueMeter, self).__init__()
+        self.reset()
+        self.val = 0
+
+    def add(self, value, n=1):
+        self.val = value
+        self.sum += value
+        self.var += value * value
+        self.n += n
+
+        if self.n == 0:
+            self.mean, self.std = np.nan, np.nan
+        elif self.n == 1:
+            self.mean = 0.0 + self.sum  # This is to force a copy in torch/numpy
+            self.std = np.inf
+            self.mean_old = self.mean
+            self.m_s = 0.0
+        else:
+            self.mean = self.mean_old + (value - n * self.mean_old) / float(self.n)
+            self.m_s += (value - self.mean_old) * (value - self.mean)
+            self.mean_old = self.mean
+            self.std = np.sqrt(self.m_s / (self.n - 1.0))
+
+    def value(self):
+        return self.mean, self.std
+
+    def reset(self):
+        self.n = 0
+        self.sum = 0.0
+        self.var = 0.0
+        self.val = 0.0
+        self.mean = np.nan
+        self.mean_old = 0.0
+        self.m_s = 0.0
+        self.std = np.nan
